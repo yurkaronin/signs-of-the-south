00413027e1bc52da8bdc9429158604d5ca249005
RoninsWork <47382042+yurkaronin@users.noreply.github.com>
2023-09-29 16:12:25 +0300

    Заканчиваю адаптив слайдера с отзывами

--- js/main.js
+++ js/main.js
@@ -73,6 +73,20 @@ document.addEventListener("DOMContentLoaded", function () {
         el: ".reviews .swiper-pagination",
         type: "fraction",
       },
+      breakpoints: {
+        // when window width is >= 320px
+        320: {
+          slidesPerView: 1
+        },
+        // when window width is >= 480px
+        767: {
+          slidesPerView: 1.4
+        },
+        // when window width is >= 640px
+        1023: {
+          slidesPerView: 2
+        }
+      }
     });
   }
 
